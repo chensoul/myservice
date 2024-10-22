0aa1a6c92f66f1e9049d6faa448fccef7aa18d8e
chensoul <ichensoul@gmail.com>
2024-10-22 16:40:14 +0800

    LoggingAspect

--- src/main/java/com/mycompany/myservice/repositories/CustomerRepository.java
+++ src/main/java/com/mycompany/myservice/repositories/CustomerRepository.java
@@ -1,6 +1,15 @@
 package com.mycompany.myservice.repositories;
 
 import com.mycompany.myservice.entities.Customer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface CustomerRepository extends JpaRepository<Customer, Long> {}
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+	@Override
+	Page<Customer> findAll(Pageable pageable);;
+
+}
