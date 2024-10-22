1f2b69efe2e9d36caac7ed8d354caf89e49a4906
chensoul <ichensoul@gmail.com>
2024-10-22 15:41:20 +0800

    refactor

--- src/main/java/com/mycompany/myservice/config/GlobalExceptionHandler.java
+++ src/main/java/com/mycompany/myservice/config/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import java.time.Instant;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
@@ -17,45 +18,49 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@Slf4j
 @ControllerAdvice
 @Order(Ordered.HIGHEST_PRECEDENCE)
 class GlobalExceptionHandler {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    ProblemDetail onException(MethodArgumentNotValidException methodArgumentNotValidException) {
-        ProblemDetail problemDetail =
-                ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(400), "Invalid request content.");
-        problemDetail.setTitle("Constraint Violation");
-        List<ApiValidationError> validationErrorsList = methodArgumentNotValidException.getAllErrors().stream()
-                .map(objectError -> {
-                    FieldError fieldError = (FieldError) objectError;
-                    return new ApiValidationError(
-                            fieldError.getObjectName(),
-                            fieldError.getField(),
-                            fieldError.getRejectedValue(),
-                            Objects.requireNonNull(fieldError.getDefaultMessage(), ""));
-                })
-                .sorted(Comparator.comparing(ApiValidationError::field))
-                .toList();
-        problemDetail.setProperty("violations", validationErrorsList);
-        return problemDetail;
-    }
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  ProblemDetail onException(MethodArgumentNotValidException methodArgumentNotValidException) {
+    ProblemDetail problemDetail =
+            ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(400), "Invalid request content.");
+    problemDetail.setTitle("Constraint Violation");
+    List<ApiValidationError> validationErrorsList = methodArgumentNotValidException.getAllErrors().stream()
+            .map(objectError -> {
+              FieldError fieldError = (FieldError) objectError;
+              return new ApiValidationError(
+                      fieldError.getObjectName(),
+                      fieldError.getField(),
+                      fieldError.getRejectedValue(),
+                      Objects.requireNonNull(fieldError.getDefaultMessage(), ""));
+            })
+            .sorted(Comparator.comparing(ApiValidationError::field))
+            .toList();
+    problemDetail.setProperty("violations", validationErrorsList);
+    return problemDetail;
+  }
 
-    @ExceptionHandler(Exception.class)
-    ProblemDetail onException(Exception exception) {
-        if (exception instanceof ResourceNotFoundException resourceNotFoundException) {
-            ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
-                    resourceNotFoundException.getHttpStatus(), resourceNotFoundException.getMessage());
-            problemDetail.setTitle("Not Found");
-            problemDetail.setType(URI.create("http://api.myservice.com/errors/not-found"));
-            problemDetail.setProperty("errorCategory", "Generic");
-            problemDetail.setProperty("timestamp", Instant.now());
-            return problemDetail;
-        } else {
-            return ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(500), exception.getMessage());
-        }
-    }
+  @ExceptionHandler(Exception.class)
+  ProblemDetail onException(Exception exception) {
+    log.error("An unexpected error occurred", exception);
 
-    record ApiValidationError(String object, String field, Object rejectedValue, String message) {}
+    if (exception instanceof ResourceNotFoundException resourceNotFoundException) {
+      ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+              resourceNotFoundException.getHttpStatus(), resourceNotFoundException.getMessage());
+      problemDetail.setTitle("Not Found");
+      problemDetail.setType(URI.create("http://api.myservice.com/errors/not-found"));
+      problemDetail.setProperty("errorCategory", "Generic");
+      problemDetail.setProperty("timestamp", Instant.now());
+      return problemDetail;
+    } else {
+      return ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(500), exception.getMessage());
+    }
+  }
+
+  record ApiValidationError(String object, String field, Object rejectedValue, String message) {
+  }
 }
